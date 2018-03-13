8f1291d1b52a0f6062879b3e92da2fc7184383d8
Giuliano Moro <gmfredricksen@GmFredricksen-MBP.local>
2018-03-13 01:42:37 +0100

    implement search functionality
    Why?
    *
    
    Issue:
    *   #3

--- src/components/SearchPage/index.jsx
+++ src/components/SearchPage/index.jsx
@@ -1,29 +1,81 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import * as BooksAPI from '../../utils/BooksAPI';
+import SearchBar from '../SearchBar';
+import Book from '../Book';
 import './SearchPage.css';
 
 class SearchPage extends Component {
+  state = {
+    filterText: '',
+    emptySearchMessage: 'Nothing searched',
+    searchedBooks: [],
+  }
+
+  handleFilterTextChange = (event) => {
+    const filterText = event.target.value;
+    console.log('handleFilterTextChange =>', filterText);
+    this.setState({ filterText });
+    if (filterText) {
+      BooksAPI.search(filterText)
+        .then((searchedBooks) => {
+          if (!searchedBooks.hasOwnProperty('error')) {
+            this.setState({ searchedBooks });
+          } else {
+            this.setState({
+              searchedBooks: [],
+              emptySearchMessage: 'Unfortunately your search did not match any book'
+            });
+          }
+        })
+    } else {
+      this.setState({
+        emptySearchMessage: 'Nothing searched',
+        searchedBooks: [],
+      });
+    }
+  }
+
+  updateBookshelf = (bookToBeUpdated, updatedShelf) => {
+    // const { allBooks } = this.state;
+
+    BooksAPI.update(bookToBeUpdated, updatedShelf);
+      // .then(() => {
+        // TODO: how to use the returned arrays?
+        // TODO: outsource this method into a utils class
+        // const updatedBooks = allBooks.map((book) => {
+        //   if (book.id === bookToBeUpdated.id) {
+        //     book.shelf = updatedShelf;
+        //   }
+        //   return book;
+        // });
+        // this.setState({ allBooks: updatedBooks });
+      // });
+  }
+
   render() {
+    console.log(this.props);
+    const { emptySearchMessage, filterText, searchedBooks } = this.state;
+
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          {/* <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a> */}
           <Link to="/" className="close-search" />
-          <div className="search-books-input-wrapper">
-            {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-            <input type="text" placeholder="Search by title or author" />
-
-          </div>
+          <SearchBar
+            filterText={filterText}
+            onFilterTextChange={this.handleFilterTextChange}
+          />
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          <ol className="books-grid">
+            { 
+              searchedBooks.length
+              ?
+              searchedBooks.map((book) => <Book key={book.id} book={book} updateBookshelf={this.updateBookshelf} />)
+              :
+              <li>{emptySearchMessage}</li>
+            }
+          </ol>
         </div>
       </div>
     )
