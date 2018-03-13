be3f56e30b2f8ec4f872f28b8a54244a82667703
Giuliano Moro <gmfredricksen@GmFredricksen-MBP.local>
2018-03-13 22:50:40 +0100

    fix: implement cover placeholder
    Why?
    *   to be used when books covers are not available
    
    Issue:
    *   #3

--- src/components/Book/BookCover.jsx
+++ src/components/Book/BookCover.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as CoverPlaceholder from './book-cover-placeholder.jpg';
 
 function BookCover (props) {
   const { bookTitle, imageLinks } = props;
@@ -8,7 +9,7 @@ function BookCover (props) {
       className="book-cover"
       style={{ width: 128, height: 193 }}
     >
-      <img width="100%" height="100%" src={imageLinks.smallThumbnail} alt={bookTitle} />
+      <img width="100%" height="100%" src={imageLinks ? imageLinks.smallThumbnail : CoverPlaceholder} alt={bookTitle} />
     </div>
   )
 }
