c15c530b24cbb81fb4a47a023f9e8479c977b45c
Giuliano Moro <gmfredricksen@GmFredricksen-MBP.local>
2018-03-13 01:42:02 +0100

    implement SearchBar component
    Why?
    *
    
    Issue:
    *   #3

---
+++ src/components/SearchBar/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function SearchBar(props) {
+  const { filterText, onFilterTextChange } = props;
+
+  return (
+    <div className="search-books-input-wrapper">
+      <input
+        type="text"
+        placeholder="Search by title or author"
+        value={filterText}
+        onChange={onFilterTextChange}
+      />
+
+    </div>
+  )
+}
+
+export default SearchBar;
